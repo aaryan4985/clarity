ad170c0eb16614713862fbf9b441912db89ea081
Aaryan Pradhan <pradhanaaryan@gmail.com>
2025-07-04 16:33:19 +0530

    env key shifted

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -114,7 +114,7 @@ const Dashboard = () => {
     setLoading(true);
     
     try {
-      const GEMINI_API_KEY = 'AIzaSyA8uYtQyob8dUJ9_Bqe_adsdSKzu2M0Al8';
+      const GEMINI_API_KEY = import.meta.env.VITE_GEMINI_API_KEY;
       
       if (!GEMINI_API_KEY) {
         throw new Error('API key not found.');
